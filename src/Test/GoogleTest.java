04298f7557461557c542748d8289507c6f3b256b
Payal <P2Test93>
2025-04-28 13:46:39 +0530

    Run

---
+++ src/Test/GoogleTest.java
@@ -0,0 +1,25 @@
+package Test;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class GoogleTest {
+
+	@Test   // TestNG will detect and run this method
+    public void openGoogle() {
+
+        // Set up WebDriver
+        System.setProperty("webdriver.chrome.driver", "E:\\chromedriver_win32\\chromedriver.exe"); 
+        
+        WebDriver driver = new ChromeDriver();
+        
+        driver.manage().window().maximize();
+        
+        driver.get("https://www.google.com");   // Optional: Open Google to test
+
+        // Close the browser
+        driver.quit();
+    }
+}
